31bb55dd3253eefb6f58c764aec5bcc9028e1115
Ann <paoletta0011@gmail.com>
2015-12-19 08:42:45 +0200

    Lesson Brandi

---
+++ Lesson11_Brandi/js/map.js
@@ -0,0 +1,26 @@
+var myLatlng = new google.maps.LatLng(49.426215, 32.094889);
+var mapOptions = {
+    zoom: 15,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+var markerImage = new google.maps.MarkerImage(
+    'images/marker.png',
+    new google.maps.Size(40,40),
+    new google.maps.Point(0,0),
+    new google.maps.Point(0,33)
+);
+var marker = new google.maps.Marker({
+    icon: markerImage,
+    position: myLatlng,
+    map: map,
+});
+google.maps.event.addListener(marker, 'mouseout', function() {
+    marker.setIcon(markerImage);
+});
+marker.setMap(map);
+mapOptions = {
+    scrollwheel: false
+};
+var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
