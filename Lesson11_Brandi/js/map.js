c7da772c9872de2117c7e13164eb3b64aef3f6ff
Ann <paoletta0011@gmail.com>
2015-12-20 14:30:25 +0200

    Lesson_Brandi

--- Lesson11_Brandi/js/map.js
+++ Lesson11_Brandi/js/map.js
@@ -1,4 +1,6 @@
+
 var myLatlng = new google.maps.LatLng(49.426215, 32.094889);
+
 var mapOptions = {
     zoom: 15,
     center: myLatlng,
@@ -23,4 +25,27 @@ marker.setMap(map);
 mapOptions = {
     scrollwheel: false
 };
+var styles = [
+    {
+        stylers: [
+            { hue: "#F5F5DC"},
+            { saturation: -70 }
+        ]
+    },{
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+            { lightness: 100 },
+            { visibility: "simplified" }
+        ]
+    },{
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
+            { visibility: "off" }
+        ]
+    }
+];
+
+map.setOptions({styles: styles});
 var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
---
+++ Lesson11_Brandi/js/slider.js
@@ -0,0 +1,7 @@
+var glide = $('.slider').glide().data('api_glide');
+
+$(window).on('keyup', function (key) {
+    if (key.keyCode === 13) {
+        glide.jump(3, console.log('Wooo!'));
+    };
+});
